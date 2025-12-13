10609c92b161a31d75fbcef65f30c8c0584d115c
bariseroglu <baris.eroglu@gmail.com>
2025-12-13 21:34:15 +0300

    initial commit

---
+++ src/main/java/com/thy/airlineroutemanager/AirlineRouteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.thy.airlineroutemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineRouteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineRouteManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thy/airlineroutemanager/AirlineRouteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thy.airlineroutemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineRouteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
